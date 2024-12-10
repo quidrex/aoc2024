803e46ec4ee70de8aa341e5204e1049e5bae2344
Gian Perrone <git@gian-perrone.de>
2024-12-10 12:41:21 +0100

    add day10 solution and new harness

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,56 @@
+pub mod util;
+
+#[macro_export]
+macro_rules! aoc_day {
+    ($t:ty, $expected_a:expr, $expected_b:expr) => {
+        fn main() {
+            let test_input = include_str!("test.txt");
+            let main_input = include_str!("main.txt");
+            $crate::run_aoc_day::<$t>(test_input, main_input, $expected_a, $expected_b);
+        }
+    };
+}
+
+pub trait AocDay {
+    fn from(input: &str) -> Self;
+    fn a(&self) -> i64;
+    fn b(&self) -> i64;
+}
+
+pub fn run_aoc_day<T: AocDay>(
+    test_input: &str,
+    main_input: &str,
+    expected_a: i64,
+    expected_b: i64,
+) {
+    let test = T::from(&test_input);
+    let main = T::from(&main_input);
+
+    let test_a = test.a();
+    let test_a_success = test_a == expected_a;
+    println!(
+        "Test A: {} {} {}",
+        test_a,
+        if test_a_success { "==" } else { "!=" },
+        expected_a
+    );
+    if test_a_success {
+        println!("Main B: {}", main.a());
+
+        let test_b = test.b();
+        let test_b_success = test_b == expected_b;
+        println!(
+            "Test B: {} {} {}",
+            test_b,
+            if test_b_success { "==" } else { "!=" },
+            expected_b
+        );
+        if test_b_success {
+            println!("Main B: {}", main.b());
+        }
+    }
+}
+
 #[macro_export]
 macro_rules! init {
     ($expected_a:expr, $expected_b:expr) => {
